1840e854e00998cd614e701409764ca3604737a4
lowes2 <maxwellwang84@gmail.com>
2026-02-13 19:35:05 -0500

    added card apop animation

--- card.gd
+++ card.gd
@@ -20,15 +20,21 @@ var grow = true
 
 #placement
 var selected = false
+@onready var particles = $CPUParticles2D
+@onready var particlesFloor = $CPUParticles2D/CPUParticles2D
+@onready var texture = $TextureRect
+var moveParticles = false
 
 func _ready():
 	battle = get_parent()
 	pivot_offset = size / 2
 	saveScale = scale
 
-func use(enemy):
-	enemy.hurt(damage)
-	battle.adjustInflammation(inflame)
+func use():
+	move(Vector2(300, -300))
+	
+	#enemy.hurt(damage)
+	#battle.adjustInflammation(inflame)
 	
 func _process(delta: float) -> void:
 	if isHovering:
@@ -39,9 +45,27 @@ func _process(delta: float) -> void:
 			scale -= Vector2(scaleCurve.sample(1-percent),scaleCurve.sample(1-percent)) / Vector2(10,10)
 	if scale < saveScale:
 		scale = saveScale
+	if moveParticles:
+		particles.global_position.y += 5
+		print(particles.global_position.y)
+		if particles.global_position.y > global_position.y + size.y + size.y + size.y:
+			moveParticles = false
+			particles.emitting = false 
+			particlesFloor.emitting = false
+			queue_free()
 		
-		
-		
+
+
+func move(targetPos):
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", targetPos, 0.3)
+	tween.tween_callback(Callable(self, "destroy"))
+	
+func destroy():
+	texture.modulate = Color(0.0, 0.0, 0.0, 1.0)
+	particles.emitting = true
+	particlesFloor.emitting = true
+	moveParticles = true
 
 func hover():
 	if not timer.is_stopped():
@@ -70,8 +94,8 @@ func _input(event):
 		hover()
 		z_index -= 1
 		grow = false
-	if event.is_action_pressed("USE"):
-		use(battle.getEnemy())
+	if event.is_action_pressed("USE") and selected:
+		use()
 
 func _on_pressed() -> void:
 	selected = true
